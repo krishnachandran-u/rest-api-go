d192e1ac6a2ede057cfee61225cf6e560c2c565b
krishnachandran-u <kriss52450@gmail.com>
2024-02-20 16:49:14 +0530

    GET getAllUsers implement

--- main.go
+++ main.go
@@ -1,9 +1,49 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+type User struct {
+	Email    string `json:"email"`
+	Name     string `json:"name"`
+	Password string `json:"password"`
+}
+
+var users = []User{
+	{Email: "johndoe@gmail.com", Name: "John Doe", Password: "johndoepassword"},
+	{Email: "steveaustin@gmai.com", Name: "Steve Austin", Password: "steveaustinpassword"},
+	{Email: "emilywilliams@gmail.com", Name: "Emily Williams", Password: "emilywilliamspassword"},
+}
+
+func getAllUsers(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, users)
+}
+
+/*
+func handleLogin(c *gin.Context) {
+	var loginData struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	if err := c.BindJSON(&loginData); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	for _, user := range users {
+		if(user.Email == loginData.Email && user.Password == loginData.Password) {
+			c.intendedJSON()
+		}
+	}
+}
+*/
+
 func main() {
-	fmt.Println("Hello, World!")
+	router := gin.Default()
+	router.GET("api/users", getAllUsers)
+	router.Run("localhost:8080")
 }
