9a1e80b827e61f06df23d4cd8211d1da307c9720
krishnachandran-u <kriss52450@gmail.com>
2024-02-20 17:22:07 +0530

    create login using jwt token

--- main.go
+++ main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 )
+
+var jwtKey = []byte("my_secret")
 
 type User struct {
 	Email    string `json:"email"`
 	Name     string `json:"name"`
 	Password string `json:"password"`
+}
+
+type Claims struct {
+	Email string `json:"email"`
+	jwt.StandardClaims
 }
 
 var users = []User{
@@ -22,7 +31,6 @@ func getAllUsers(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, users)
 }
 
-/*
 func handleLogin(c *gin.Context) {
 	var loginData struct {
 		Email    string `json:"email"`
@@ -35,15 +43,34 @@ func handleLogin(c *gin.Context) {
 	}
 
 	for _, user := range users {
-		if(user.Email == loginData.Email && user.Password == loginData.Password) {
-			c.intendedJSON()
+		if user.Email == loginData.Email && user.Password == loginData.Password {
+
+			expirationTime := time.Now().Add(24 * time.Hour)
+
+			claims := &Claims{
+				Email: loginData.Email,
+				StandardClaims: jwt.StandardClaims{
+					ExpiresAt: expirationTime.Unix(),
+				},
+			}
+
+			token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+			tokenString, err := token.SignedString(jwtKey)
+
+			if err != nil {
+				c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": "could not login"})
+				return
+			}
+
+			c.IndentedJSON(http.StatusOK, gin.H{"token": tokenString})
+			return
 		}
 	}
 }
-*/
 
 func main() {
 	router := gin.Default()
 	router.GET("api/users", getAllUsers)
+	router.POST("api/login", handleLogin)
 	router.Run("localhost:8080")
 }
